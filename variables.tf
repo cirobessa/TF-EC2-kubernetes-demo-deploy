e09f5064c1d19091839c921a8c9d11b42e99e87d
Ciro Bessa <60109116+cirobessa@users.noreply.github.com>
2024-09-17 15:10:32 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,13 @@ variable "kubecname" {
   description = "Name assign to the cluster"
   default     = ""
 }
+
+
+
+variable "AWS_ACCESS_KEY_ID" {
+  default = ""
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  default = ""
+}
